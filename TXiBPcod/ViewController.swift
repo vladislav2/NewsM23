2524939e0db7f0ad2bac6e865a3f8e0895f399f4
vladislav2 <etter.id75@gmail.com>
2020-12-30 18:21:04 +0200

    Initial Commit

---
+++ TXiBPcod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TXiBPcod
+//
+//  Created by user on 30.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
